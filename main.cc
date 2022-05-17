0dedc66428be3981e17718713b20410d72388421
Kourosh Alasti <kouroshalastibusiness@gmail.com>
2022-05-16 22:43:18 -0700

    update

--- main.cc
+++ main.cc
@@ -11,6 +11,8 @@ int main() {
     string* list;
     list = &arr_one[0];
 
+    cout << list << endl;
+
     for(int i = 0; i < (sizeof(arr_two) / sizeof(string)); i++){
         void FindSubstring(string word_one, string word_two);
         FindSubstring(arr_one[0], arr_two[i]);
